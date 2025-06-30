33797748f6fbb541851addff0839b95a17ecd216
Phantom00716 <murodkhon2007@gmail.com>
2025-06-30 14:40:25 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -31,10 +31,12 @@ async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def main():
     app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("info", info))
     app.add_handler(CommandHandler("cottages", cottages))
     app.add_handler(CommandHandler("contact", contact))
+
     await app.run_polling()
 
 if __name__ == "__main__":
